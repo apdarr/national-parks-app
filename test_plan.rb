78b0f11f67bb595c0e84e91b32daef326af0c0d9
apdarr <alexdarr@gmail.com>
2017-04-06 16:58:51 -0400

    add jmeter stuff

--- test_plan.rb
+++ test_plan.rb
@@ -2,4 +2,4 @@ test do
     thread count: 10 do
       visit name: 'National Parks Home Page', url: 'https://national-parks-app.herokuapp.com'
     end
-end.jmx
+end.run(path: '/vendor/apache-jmeter/bin/')
