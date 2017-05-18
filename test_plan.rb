20108b34a6e1eb00f87f78a6915f0ad171930a61
apdarr <alexdarr@gmail.com>
2017-05-18 11:47:11 -0400

    add jmeter

--- test_plan.rb
+++ test_plan.rb
@@ -5,19 +5,19 @@ test do
     threads count: 2 do
       #transaction 'vist_home_page' do
 
-
-
       visit name: "Parks", url: "https://national-parks-app-prod.herokuapp.com/" do
       end
 
-      submit url: "https://national-parks-app-prod.herokuapp.com/parks"
+      vist name: "Parks/New" url: "https://national-parks-app.herokuapp.com/parks/new" do
         extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
-        fill_in: {
-          'utf8' => '%E2%9C%93',
-          'authenticity_token' => '${csrf-token}',
-          'park[name]' => 'Yellowstone',
-          'park[journal]' => 'Sunny, warm',
-          'commit' => 'Create Park'
-        }
+        submit url: "https://national-parks-app-prod.herokuapp.com/parks",
+          fill_in: {
+            'utf8' => '%E2%9C%93',
+            'authenticity_token' => '${csrf-token}',
+            'park[name]' => 'Yellowstone',
+            'park[journal]' => 'Sunny, warm',
+            'commit' => 'Create Park'
+          }
+      end
     end
 end.run(path:'/app/vendor/apache-jmeter/apache-jmeter-3.1/bin/')
