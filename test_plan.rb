bc19982cd8597c8547eb19fc75a0c67b0c296a0c
apdarr <alexdarr@gmail.com>
2017-05-18 11:18:47 -0400

    add jmeter

--- test_plan.rb
+++ test_plan.rb
@@ -7,7 +7,7 @@ test do
       #transaction 'vist_home_page' do
           visit name: "Parks", url: "https://national-parks-app-prod.herokuapp.com/" do
             extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
-            puts csrf-token
+            puts "${csrf-token}"
             submit url: "https://national-parks-app-prod.herokuapp.com/parks",
               fill_in: {
                 'utf8': '%E2%9C%93',
