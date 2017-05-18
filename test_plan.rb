503364994b505acd78ef82d7fb0824171fd1cfde
apdarr <alexdarr@gmail.com>
2017-05-18 14:16:51 -0400

    a

--- test_plan.rb
+++ test_plan.rb
@@ -9,17 +9,16 @@ test do
       #  visit name: "Parks/New", url: "https://national-parks-app-prod.herokuapp.com/parks/new" do
       #    extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
       #  end
-      get name: "Parks/New", url: "https://national-parks-app-prod.herokuapp.com/parks/new"
-
-      submit name: "adding Parks", url: "https://national-parks-app-prod.herokuapp.com/parks",
-        fill_in: {
-          'utf8' => '%E2%9C%93',
-          'authenticity_token' => '${csrf-token}',
-          'park[name]' => 'Yellowstone',
-          'park[journal]' => 'Sunny, warm',
-          'commit' => 'Create Park'
-        } do
-          extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
+      visit name: "Parks/New", url: "https://national-parks-app-prod.herokuapp.com/parks/new" do
+        extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
+        submit name: "adding Parks", url: "https://national-parks-app-prod.herokuapp.com/parks",
+          fill_in: {
+            'utf8' => '%E2%9C%93',
+            'authenticity_token' => '${csrf-token}',
+            'park[name]' => 'Yellowstone',
+            'park[journal]' => 'Sunny, warm',
+            'commit' => 'Create Park'
+          }
       end
     end
 end.run(path:'/app/vendor/apache-jmeter/apache-jmeter-3.1/bin/')
