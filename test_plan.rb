d539dc668033460956d7af906c72b76009ce14b8
apdarr <alexdarr@gmail.com>
2017-05-18 11:52:54 -0400

    add jmeter

--- test_plan.rb
+++ test_plan.rb
@@ -6,18 +6,18 @@ test do
       #transaction 'vist_home_page' do
 
       visit name: "Parks", url: "https://national-parks-app-prod.herokuapp.com/" do
-      end
 
-      visit name: "Parks/New", url: "https://national-parks-app.herokuapp.com/parks/new" do
-        extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
-        submit url: "https://national-parks-app-prod.herokuapp.com/parks",
-          fill_in: {
-            'utf8' => '%E2%9C%93',
-            'authenticity_token' => '${csrf-token}',
-            'park[name]' => 'Yellowstone',
-            'park[journal]' => 'Sunny, warm',
-            'commit' => 'Create Park'
-          }
+        visit name: "Parks/New", url: "https://national-parks-app.herokuapp.com/parks/new" do
+          extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
+          submit url: "https://national-parks-app-prod.herokuapp.com/parks",
+            fill_in: {
+              'utf8' => '%E2%9C%93',
+              'authenticity_token' => '${csrf-token}',
+              'park[name]' => 'Yellowstone',
+              'park[journal]' => 'Sunny, warm',
+              'commit' => 'Create Park'
+            }
+        end
       end
     end
 end.run(path:'/app/vendor/apache-jmeter/apache-jmeter-3.1/bin/')
