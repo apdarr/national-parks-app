d40ab7afe5de873d481190fddbc901166ed6613e
apdarr <alexdarr@gmail.com>
2017-05-18 11:11:25 -0400

    add jmeter

--- test_plan.rb
+++ test_plan.rb
@@ -1,7 +1,21 @@
 require 'ruby-jmeter'
+# Targeting seperate Heroku app
+
 
 test do
-    thread count: 10 do
-      visit name: 'National Parks Home Page', url: 'https://national-parks-app.herokuapp.com'
+    threads count: 5 do
+      #transaction 'vist_home_page' do
+          visit name: "Parks", url: "https://national-parks-app-prod.herokuapp.com/" do
+            extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
+            puts csrf-token
+            submit url: "https://national-parks-app-prod.herokuapp.com/parks",
+              fill_in: {
+                'utf8': '%E2%9C%93',
+                'authenticity_token': '${csrf-token}',
+                'park[name]': 'Yellowstone',
+                'park[journal]': 'Sunny, warm',
+                'commit': 'Create Park'
+              }
+          end
     end
-end.run(path: '/vendor/apache-jmeter/bin/')
+end.run(path:'/app/vendor/apache-jmeter/apache-jmeter-3.1/bin/')
