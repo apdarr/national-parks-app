af6cd1bb603b7bd8765d6aad1d1045c1094bdfc2
apdarr <alexdarr@gmail.com>
2017-05-18 11:28:10 -0400

    change it

--- test_plan.rb
+++ test_plan.rb
@@ -1,13 +1,11 @@
 require 'ruby-jmeter'
 # Targeting seperate Heroku app
 
-
 test do
-    threads count: 5 do
+    threads count: 2 do
       #transaction 'vist_home_page' do
           visit name: "Parks", url: "https://national-parks-app-prod.herokuapp.com/" do
-            extract regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
-            puts "#{csrf-token}"
+            web_reg_save_param regex: "content='(.+?)' name='csrf-token'", name: 'csrf-token'
             submit url: "https://national-parks-app-prod.herokuapp.com/parks",
               fill_in: {
                 'utf8' => '%E2%9C%93',
