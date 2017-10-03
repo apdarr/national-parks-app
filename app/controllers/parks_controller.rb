3006a9dee9a6752b294165443ef95eb7baeecdb7
apdarr <alexdarr@gmail.com>
2017-10-03 16:10:41 -0400

    better oneoff

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -6,7 +6,7 @@ class ParksController < ApplicationController
   def index
     @parks = Park.all
     puts "Testing heroku CLI in a dyno"
-    %x{heroku run bundle exec rake db:migrate}
+    %x{heroku run ruby hello_world.rb}
   end
 
   # GET /parks/1
